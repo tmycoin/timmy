b47e7687dae8f134d78fb7e6eb0df6d4d3b48c22
Timmy <hi@timmycoin.net>
2019-01-14 09:09:17 +0300

    P2P, RPC changed

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -90,8 +90,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  17230;
-const int      RPC_DEFAULT_PORT                              =  18230;
+const int      P2P_DEFAULT_PORT                              =  1302;
+const int      RPC_DEFAULT_PORT                              =  1303;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
